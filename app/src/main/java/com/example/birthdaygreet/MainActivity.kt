1cae733215d042353fbd82aaf7d0b6bc4686c357
Ayush <ayush8402@gmail.com>
2021-08-24 21:01:26 +0530

    passed intent and message

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.birthdaygreet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
@@ -13,8 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun createButton(view: View) {
+
+        val name = findViewById<EditText>(R.id.nameInput).editableText.toString()
+
         val intent = Intent(this, WishScreen::class.java)
 
+
+        intent.putExtra("name_extra", name)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/birthdaygreet/WishScreen.kt
+++ app/src/main/java/com/example/birthdaygreet/WishScreen.kt
@@ -2,10 +2,19 @@ package com.example.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class WishScreen : AppCompatActivity() {
+
+    companion object{
+        const val NAME_EXTRA = "name_extra"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wish_screen)
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+
+        findViewById<TextView>(R.id.wish).text = "Happy Birthday $name"
     }
 }
